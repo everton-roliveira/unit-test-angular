c430174bba6deed4c4dc6ed63d67b180de28af2c
Everton Roberto <everton_roliveira@outlook.com>
2020-02-14 13:14:09 -0300

    Ädicionado a página de criar e editar usuário, e o formulário

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,8 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { UserRoutingModule } from './user-routing.module';
 import { ClarityModule } from '@clr/angular';
+import { ComponentsComponent } from './components/components.component';
+import { FormComponent } from './components/form/form.component';
+import { EditComponent } from './edit/edit.component';
+import { CreateComponent } from './create/create.component';
 @NgModule({
-  declarations: [ListComponent],
+  declarations: [ListComponent, ComponentsComponent, FormComponent, EditComponent, CreateComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
