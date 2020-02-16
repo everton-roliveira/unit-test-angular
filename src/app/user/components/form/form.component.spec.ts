12a9acde5d0e69e580a2c5ca2157843b9e225d5c
Everton Roberto <everton_roliveira@outlook.com>
2020-02-16 08:43:13 -0300

    adicionado modules de imports do framework clarity no test, dfescribe explicação

--- src/app/user/components/form/form.component.spec.ts
+++ src/app/user/components/form/form.component.spec.ts
@@ -1,16 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
+import { ClrRadioModule, ClrIconModule, ClrInputModule, ClrCheckboxModule } from '@clr/angular';
 
-describe('FormComponent', () => {
+// imports do framework clarity
+const CLARITY_MODULES = [
+  ClrRadioModule,
+  ClrIconModule,
+  ClrInputModule,
+  ClrCheckboxModule
+]
+
+// fdescribe -> usado para fixar o run de teste em um unico describe
+fdescribe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      declarations: [FormComponent],
+      imports: [
+        // adicionado o array de imports do framework
+        CLARITY_MODULES
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
