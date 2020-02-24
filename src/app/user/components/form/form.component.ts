81256c2a3d486b82eebbab6d0d9a8e801538560b
Everton Roberto <everton_roliveira@outlook.com>
2020-02-24 09:49:16 -0300

    Adicionado os controles e validacões no html

--- src/app/user/components/form/form.component.ts
+++ src/app/user/components/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/shared/models/user';
 import { genderValidator } from '../../../shared/directives/gender.directive';
+import { GenderEnum } from 'src/app/shared/enum/gender.enum';
 
 const EMAIL_PARTTERN = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
 
@@ -24,6 +25,16 @@ export class FormComponent implements OnInit {
     this.createForm(this.user);
   }
 
+  // O escopo do modelo é limitado aos membros da instância do componente, 
+  // por isso preciso criar um get para o enum
+  get GenderEnum() {
+    return GenderEnum;
+  }
+
+  validateForm() {
+    this.emitForm.emit(this.form.valid ? this.form.value : null);
+  }
+
   private createForm(user: User) {
     this.form = this._fb.group({
       'name': [user.name, [
@@ -40,9 +51,5 @@ export class FormComponent implements OnInit {
       ],
       'status': [user.status]
     });
-  }
-
-  validateForm() {
-    this.emitForm.emit(this.form.valid ? this.form.value : null);
   }
 }
